6fff4ebebb18968374d78711c6e00dd92a95c456
FarhadF <farhad.farahi@gmail.com>
2017-05-28 17:57:53 +0430

    refactoring complete

--- controllers/movies.go
+++ controllers/movies.go
@@ -16,47 +16,21 @@ type errOut struct {
 
 func GetMovies(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	/*	authToken, err := token.ExtractToken(r)
 
-		if err != nil {
-			errout := new(errOut)
-			errout.Error = err.Error()
-			w.WriteHeader(http.StatusBadRequest)
-			if err := json.NewEncoder(w).Encode(errout); err != nil {
-				log.Panic("Error EncodingJson in ControllersGetMovies", err)
-			}
-		} else {
-			tokenStatus, err := token.ParseToken(authToken)
-			if err != nil || tokenStatus == false {
-				errout := new(errOut)
-				errout.Error = err.Error()
-				w.WriteHeader(http.StatusForbidden)
-				if err := json.NewEncoder(w).Encode(errout); err != nil {
-					log.Panic("Error EncodingJson in ControllersGetMovies", err)
-				}
-				log.Println("token status err: ", err)
+	if err := json.NewEncoder(w).Encode(models.GetMovies()); err != nil {
+		log.Panic("Error EncodingJson in ControllersGetMovies", err)
 
-			} else {
-
-				//w.Header().Set("Access-Control-Allow-Origin", "*")
-				if err := json.NewEncoder(w).Encode(models.GetMovies()); err != nil {
-					log.Panic("Error EncodingJson in ControllersGetMovies", err)
-				}
-			}
-		}*/
-	status := token.TokenHandler(w, r)
-	if status == true {
-		if err := json.NewEncoder(w).Encode(models.GetMovies()); err != nil {
-			log.Panic("Error EncodingJson in ControllersGetMovies", err)
-		}
 	}
 }
 
 func NewMovie(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	//w.Header().Set("Access-Control-Allow-Origin", "*")
-	if err := json.NewEncoder(w).Encode(models.NewMovie(r)); err != nil {
-		log.Panic("Error EncodingJson in ControllersNewMovie", err)
+	status := token.TokenHandler(w, r)
+	if status == true {
+		if err := json.NewEncoder(w).Encode(models.NewMovie(r)); err != nil {
+			log.Panic("Error EncodingJson in ControllersNewMovie", err)
+		}
 	}
 }
 
@@ -70,15 +44,21 @@ func GetMovieById(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 
 func UpdateMovie(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	if err := json.NewEncoder(w).Encode(models.UpdateMovie(r, p.ByName("id"))); err != nil {
-		log.Panic("Controller UpdateMovieById json err: ", err)
+	status := token.TokenHandler(w, r)
+	if status == true {
+		if err := json.NewEncoder(w).Encode(models.UpdateMovie(r, p.ByName("id"))); err != nil {
+			log.Panic("Controller UpdateMovieById json err: ", err)
+		}
 	}
 }
 
 func DeleteMovie(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	//w.Header().Set("Access-Control-Allow-Origin", "*")
-	if err := json.NewEncoder(w).Encode(models.DeleteMovie(p.ByName("id"))); err != nil {
-		log.Panic("Controller UpdateMovieById json err: ", err)
+	status := token.TokenHandler(w, r)
+	if status == true {
+		if err := json.NewEncoder(w).Encode(models.DeleteMovie(p.ByName("id"))); err != nil {
+			log.Panic("Controller UpdateMovieById json err: ", err)
+		}
 	}
 }
